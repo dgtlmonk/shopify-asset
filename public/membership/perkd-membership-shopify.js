767b3c06594735a00a5c8b88cb53d003a0a531fe
dgtlmonk <dgtlmonk@gmail.com>
2021-08-17 19:04:48 +0800

    chore: rename rewards to membership

--- public/membership/perkd-membership-shopify.js
+++ public/membership/perkd-membership-shopify.js
@@ -19,7 +19,7 @@ function loadDeferScript(src) {
 }
 
 (async () => {
-  // loadScript(`${assetRoot}/perkd-rewards-init.js`);
+  // loadScript(`${assetRoot}/perkd-membership-init.js`);
 
   function getElById(id) {
     return document.getElementById(id);
@@ -52,7 +52,7 @@ function loadDeferScript(src) {
     // TODO:  check offers
 
     // Attemp to check user offers
-    // fetch("https://perkd-dev.ngrok.io/rewards", {
+    // fetch("https://perkd-dev.ngrok.io/membership", {
     //   method: "POST",
     //   body: JSON.stringify({
     //     customerId: customerId,
@@ -77,43 +77,43 @@ function loadDeferScript(src) {
           <head>
             <meta charset="utf-8">
           	<meta content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" name="viewport">
-          	<title>Perkd Rewards</title>
-           <link rel="stylesheet" href="https://perkd-shopify-asset.vercel.app/static/perkd-rewards.css" />
+          	<title>Perkd membership</title>
+           <link rel="stylesheet" href="https://perkd-shopify-asset.vercel.app/membership/perkd-membership.css" />
           </head>
           <body>
             <div style="overflow: hidden; position: fixed; bottom: 0; right: 0">
-              <div id="perkd-rewards-widget-launcher"></div>
+              <div id="perkd-membership-widget-launcher"></div>
             </div>
          </body>
        </html>`;
 
   // TODO: Use this template for refactor
   // let el = document.createRange().createContextualFragment(`
-  //   <div class="perkd-rewards-widget-app" aria-live="polite">
-  //     <div id="perkd-rewards-widget-frame-container" style="overflow:hidden
+  //   <div class="perkd-membership-widget-app" aria-live="polite">
+  //     <div id="perkd-membership-widget-frame-container" style="overflow:hidden
   //      position:fixed; width:300px; height:300px;
   //      bottom:6px; right:6px; z-index:2147483649 !important;">
-  //   	        <iframe title="Perkd Rewards Program Launcher" id="perkd-rewards-widget-frame" scrolling="no"
+  //   	        <iframe title="Perkd membership Program Launcher" id="perkd-membership-widget-frame" scrolling="no"
   //              style="position:absolute; height:0; max-height:100%; max-width:100%; min-height:100%; min-width:100%; width:0; border:0; outline:0; right:0; bottom:0"/>
   //     </div>
-  //     <iframe allowfullscreen name="perked-widget-launcher-frame" title="Perkd Rewards"></iframe>
+  //     <iframe allowfullscreen name="perked-widget-launcher-frame" title="Perkd membership"></iframe>
   //   </div>`);
 
   // FIXME: width/height is blocking the page!
   let el = document.createRange().createContextualFragment(`
-    <div id="perkd-rewards-widget-container"
+    <div id="perkd-membership-widget-container"
           aria-live="polite"
           style="overflow:hidden; position:fixed;
                  width:300px; height:300px;
                  bottom:8px; right:8px;
                  z-index:2147483649 !important;">
-      <div class="perkd-rewards-widget-frame">
-    	  <iframe id="perkd-rewards-widget-frame"
+      <div class="perkd-membership-widget-frame">
+    	  <iframe id="perkd-membership-widget-frame"
                scrolling="no"></iframe>
       </div>
       <iframe allowfullscreen
-              id="perkd-rewards-widget-launcher-frame"
-              title="Perkd Rewards"
+              id="perkd-membership-widget-launcher-frame"
+              title="Perkd Membership"
               style="position:absolute; height:0; max-height:100%; max-width:100%;
                        min-height:100%; min-width:100%;
                        width:0; border:0; outline:0;
@@ -123,7 +123,7 @@ function loadDeferScript(src) {
     </div>`);
 
   document.body.appendChild(el);
-  const iEl = getElById("perkd-rewards-widget-launcher-frame");
+  const iEl = getElById("perkd-membership-widget-launcher-frame");
 
   if (iEl) {
     console.info("perkd iframe found");
@@ -131,7 +131,7 @@ function loadDeferScript(src) {
 
     iEl.addEventListener("load", () => {
       // TODO: Load CSS via script
-      loadAsyncScript(`${assetRoot}/perkd-rewards-widget.min.js`);
+      loadAsyncScript(`${assetRoot}/perkd-membership-widget.min.js`);
     });
   }
 })();
@@ -145,7 +145,7 @@ function loadDeferScript(src) {
 //   );
 
 // cssResource.href =
-//   "https://perkd-shopify-asset.vercel.app/static/perkd-rewards.css";
+//   "https://perkd-shopify-asset.vercel.app/static/perkd-membership.css";
 // cssResource.rel = t ? "stylesheet" : "preload";
 // cssResource.as = "style";
 // document.head.appendChild(cssResource);
