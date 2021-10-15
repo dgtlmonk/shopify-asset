683110eb06100e293eaf0f12e3573dedfec1d45d
dgtlmonk <dgtlmonk@gmail.com>
2021-10-15 22:09:14 +0800

    debug: load api (4)

--- public/membership/perkd-membership-shopify.js
+++ public/membership/perkd-membership-shopify.js
@@ -37,7 +37,9 @@ function loadDeferScript(src) {
     if (atomic) {
       console.log(" fetching data ");
       atomic(`${apiUrl}/membership/1`).then(function (res) {
-        console.log(" mock api response ", res);
+        var { data } = res;
+
+        console.log(" mock api response ", data);
       });
     }
 
